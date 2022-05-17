79154fe79cf52041a6891291484d65cd39fe2b83
Lumia-ESFD <82859034+Lumia-ESFD@users.noreply.github.com>
2022-05-17 21:41:03 +0800

    update

--- MI-R4/clone_source.sh
+++ MI-R4/clone_source.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone -b master --single-branch https://github.com/Lienol/openwrt
-mkdir -p openwrt/package/myapps
+git clone -b master --single-branch https://github.com/Lienol/openwrt OpenWRT
+mkdir -p OpenWRT/package/myapps
 
-cd openwrt/package/myapps
+cd OpenWRT/package/myapps
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
@@ -13,3 +13,16 @@ git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
+
+cd ${GITHUB_WORKSPACE}/OpenWRT/
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
+
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+
+mv -f ${GITHUB_WORKSPACE}/${{github.event.inputs.Models}}/.config ./.config
---
+++ start_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ${GITHUB_WORKSPACE}/OpenWRT/
+
+sed -i 's/^[ \t]*//g' ./.config
+make defconfig
+make download -j8 || make download -j1 V=s
+rm -rf $(find ./dl/ -size -1024c)
+make -j$(nproc) || make -j1 V=s
